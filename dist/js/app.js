ff0dd1c9c73b5a94a882b2878c7cd565509a1d16
Myroslav <myroslavtp@gmail.com>
2023-03-13 19:16:49 +0200

    dev

--- dist/js/app.js
+++ dist/js/app.js
@@ -13,54 +13,10 @@
             document.documentElement.classList.add(className);
         }));
     }
-    let bodyLockStatus = true;
-    let bodyLockToggle = (delay = 500) => {
-        if (document.documentElement.classList.contains("lock")) bodyUnlock(delay); else bodyLock(delay);
-    };
-    let bodyUnlock = (delay = 500) => {
-        let body = document.querySelector("body");
-        if (bodyLockStatus) {
-            let lock_padding = document.querySelectorAll("[data-lp]");
-            setTimeout((() => {
-                for (let index = 0; index < lock_padding.length; index++) {
-                    const el = lock_padding[index];
-                    el.style.paddingRight = "0px";
-                }
-                body.style.paddingRight = "0px";
-                document.documentElement.classList.remove("lock");
-            }), delay);
-            bodyLockStatus = false;
-            setTimeout((function() {
-                bodyLockStatus = true;
-            }), delay);
-        }
-    };
-    let bodyLock = (delay = 500) => {
-        let body = document.querySelector("body");
-        if (bodyLockStatus) {
-            let lock_padding = document.querySelectorAll("[data-lp]");
-            for (let index = 0; index < lock_padding.length; index++) {
-                const el = lock_padding[index];
-                el.style.paddingRight = window.innerWidth - document.querySelector(".wrapper").offsetWidth + "px";
-            }
-            body.style.paddingRight = window.innerWidth - document.querySelector(".wrapper").offsetWidth + "px";
-            document.documentElement.classList.add("lock");
-            bodyLockStatus = false;
-            setTimeout((function() {
-                bodyLockStatus = true;
-            }), delay);
-        }
-    };
     function menuInit() {
         if (document.querySelector(".header__burger")) document.addEventListener("click", (function(e) {
-            if (bodyLockStatus && e.target.closest(".header__burger")) {
-                bodyLockToggle();
-                document.documentElement.classList.toggle("menu-open");
-            }
-            if (e.target.closest(".header__item") || e.target.closest(".page")) {
-                bodyLockToggle();
-                document.documentElement.classList.remove("menu-open");
-            }
+            if (e.target.closest(".header__burger")) document.documentElement.classList.toggle("menu-open");
+            if (e.target.closest(".header__item") || e.target.closest(".page")) document.documentElement.classList.remove("menu-open");
         }));
     }
     function ssr_window_esm_isObject(obj) {
